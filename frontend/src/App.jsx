7e71548ea55e568b5e629c499504384b7ec558af
Virender Prasad <virender288@gmail.com>
2025-01-08 13:25:39 +0530

    commit for routes to check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,13 @@ import AddStudent from "./pages/adminDashboard/studentSection/AddStudent.jsx";
 import SeeAllStudent from "./pages/adminDashboard/studentSection/SeeAllStudent.jsx";
 import Student from "./pages/adminDashboard/studentSection/Student.jsx";
 import EditStudent from "./pages/adminDashboard/studentSection/EditStudent.jsx";
+import ViewCertificate from "./pages/adminDashboard/certificateSection/ViewCertificate.jsx";
+import CenterDocument from "./pages/adminDashboard/centerSection/CenterDocument.jsx";
+import AddMarks from "./pages/adminDashboard/offlineExamination/AddMarks.jsx";
+import AddStudentsMarks from "./pages/adminDashboard/offlineExamination/AddStudentMarks.jsx";
+import ViewMarks from "./pages/adminDashboard/offlineExamination/ViewMarks.jsx";
+import StudentsResult from "./pages/adminDashboard/offlineExamination/StudentResult.jsx";
+import CourseEnquiries from "./pages/adminDashboard/studentSection/CourseEnquiry.jsx";
 
 export const App = createBrowserRouter([
   {
@@ -33,6 +40,13 @@ export const App = createBrowserRouter([
       { path: "student/:registrationNumber", element: <StudentDashboardUI /> },
       { path: "admin", element: <AdminLoginUI /> },
       { path: "dashboard", element: <DashboardUI /> },
+      { path: "centerDocument", element: <CenterDocument/> },
+      { path: "viewCertificate", element: <ViewCertificate/> },
+      { path: "addMarks", element: <AddMarks/> },
+      { path: "add-marks/:registrationNumber", element: <AddStudentsMarks/> },
+      { path: "viewMarks", element: <ViewMarks/> },
+      { path: "studentResult/:registrationNumber", element: <StudentsResult/> },
+      { path: "courseEnquiry", element: <CourseEnquiries/> },
       { path: "dashboard/addstudents", element: <AddStudent /> },
       { path: "dashboard/seeallstudents", element: <SeeAllStudent /> },
       { path: "one-student/:registrationNumber", element: <Student /> },
